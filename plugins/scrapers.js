de4a5eb8025d6be7b0c737ae63b9e630ebebd191
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2022-01-14 10:28:01 +0530

    Update scrapers.js
    🥺🚶

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -1043,7 +1043,7 @@ else if (config.WORKTYPE == 'public') {
 
             const vcard = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n' 
-            + 'FN:' + Config.OA_NAME + '\n' //created afnanplk, please copy this with credit..
+            + 'FN:' + Config.OWNER + '\n' //created afnanplk, please copy this with credit..
             + 'ORG:Misshelena;\n' 
             + 'TEL;type=CELL;type=VOICE;waid=' + Config.PHONE + ':' + Config.PHONE + ' \n'
             + 'END:VCARD'
