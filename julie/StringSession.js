157b1e197b5e51d77a7029829ad0d90919ed8f63
SPIRIT SER <96570722+SPlRlT-YT@users.noreply.github.com>
2022-02-12 15:05:45 +0530

    Update StringSession.js

--- julie/StringSession.js
+++ julie/StringSession.js
@@ -18,7 +18,7 @@ class StringSession {
 
     deCrypt(string = undefined) {
 
-        if ('MISSHELENA_CODE' in process.env && string === undefined) {
+        if ('PRINCESER_CODE' in process.env && string === undefined) {
 
             string = process.env.STRING_SESSION;
 
@@ -46,7 +46,7 @@ class StringSession {
 
     createStringSession(dict) {
 
-        return 'MISSHELENA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'PRINCESER;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
 
     }
 
