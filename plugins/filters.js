d09a56d5556589762215d9c8be5e15f7cc520bda
PRINCE-SER <98514119+PRINCE-SER@users.noreply.github.com>
2022-02-13 08:22:01 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -128,7 +128,7 @@ await message.client.sendMessage(message.jid, fs.readFileSync('./Amalser/Prince.
 await message.client.sendMessage(message.jid, fs.readFileSync('./Prince_stickers/spirit.webp'), MessageType.sticker, { mimetype: Mimetype.webp,quoted : message.data, ptt: false})
      }
        
-const array = ['Hi','spirit','alive','Prince']
+const array = ['Hi','spirit','alive','Prince','ayin','Mm','song']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
