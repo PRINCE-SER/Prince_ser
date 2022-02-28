7f11f71745d72de5dc64ff0a8a85c9dc576a7b0e
SPIRIT SER <96570722+SPlRlT-YT@users.noreply.github.com>
2022-02-28 15:48:46 +0530

    🌝💞

--- julie/StringSession.js
+++ julie/StringSession.js
@@ -46,7 +46,7 @@ class StringSession {
 
     createStringSession(dict) {
 
-        return 'RINCESER;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'PRINCESER;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
 
     }
 
