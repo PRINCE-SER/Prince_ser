a949d5e41976edd161c9d35ddc38ffae1dadaf9a
Prince ser <98514119+PRINCE-SER@users.noreply.github.com>
2022-03-09 07:41:13 +0530

    Update reels.js

--- plugins/reels.js
+++ plugins/reels.js
@@ -21,9 +21,9 @@ Asena.addCommand({pattern: 'insta ?(.*)', fromMe: false, desc: "Downloads from i
 
     reply = await message.client.sendMessage(message.jid,UPLOAD_ING , MessageType.text, { quoted: message.data });
     
-    if (type === 'image') return await message.sendMessage(data, MessageType.image, { caption: "*Codded by Miss Helena*", quoted: message.data })
+    if (type === 'image') return await message.sendMessage(data, MessageType.image, { caption: "*Codded by prince ser*", quoted: message.data })
     
-    if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: "*Codded by Miss Helena*", quoted: message.data })
+    if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: "*Codded by prince ser*", quoted: message.data })
     
 });
     
