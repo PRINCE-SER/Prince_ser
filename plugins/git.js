165bbc39bb79c5be4297f8480a9b8dcb91387726
Prince ser <98514119+PRINCE-SER@users.noreply.github.com>
2022-03-22 10:55:54 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,12 +7,12 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/BY13nKv.jpeg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/C4ego0A.jpeg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `Pʀɪɴᴄᴇ Sᴇʀ Bᴏᴛ💝
 ᴄʀᴇᴀᴛᴏʀ ɴᴜᴍʙᴇʀ: wa.me/918590565968
 
-ᴄʀᴇᴀᴛᴏʀ ɴᴜᴍʙᴇʀ: wa.me/917994093863
+ᴄʀᴇᴀᴛᴏʀ ɴᴜᴍʙᴇʀ: wa.me/919446652312
 
 Pʀɪɴᴄᴇ ʙᴏᴛ ɢʀᴏᴜᴘ: https://chat.whatsapp.com/FH8pCHTfpCMFkHbWVGVaLv
 
@@ -23,7 +23,7 @@ Aᴜᴅɪᴏ ᴄᴏᴍᴍᴀᴅᴇ :   https://github.com/PRINCE-SER/Prince_ser/
 Sᴛɪᴄᴋᴇʀ ᴄᴏᴍᴍᴀᴅᴇ: https://github.com/PRINCE-SER/Prince_ser/tree/master/Prince_stickers
 
 
-💝Pʀɪɴᴄᴇ sᴇʀ Bᴏᴛ Cʀᴇᴀᴛᴇᴅ Bʏ ᴘʀɪɴᴄᴇ ᴀɴᴅ sᴘɪʀɪᴛ💝
+💝Cʀᴇᴀᴛᴇᴅ Bʏ ᴘʀɪɴᴄᴇ sᴇʀ💝
 `}) 
 
 }));
